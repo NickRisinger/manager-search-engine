d655c74f6d5152fa943b51c98d93f71c5ce4d945
Nick Risinger <97955733+NickRisinger@users.noreply.github.com>
2023-03-01 11:28:51 +0000

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    // exposeConfig: false,
+    // config: {},
+    // injectPosition: 0,
+    // viewer: true,
+  },
+  modules: ['@nuxtjs/tailwindcss'],
+});
